58b87178d5eb2f8c96d93a0db39757b9adc99c92
alassane8 <alassane.wade@edu.ece.fr>
2026-06-07 17:21:15 +0200

    fix deleting useless create_directories function

--- code/create_directories.py
+++
@@ -1,6 +0,0 @@
-def create_directories(base_path, directories):
-    for directory in directories:
-        dir_path = base_path / directory
-        if not dir_path.exists():
-            dir_path.mkdir()
-            print(f"Created directory: {dir_path}")
